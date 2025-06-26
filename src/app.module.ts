c596813610806b053e4c2301c5adeef6b0171b09
caveman752736 <caveman752736@gmail.com>
2025-06-26 23:29:27 +0900

    Fixed postgres implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './modules/user/user.entity';
 import { Room } from './modules/room/room.entity';
 import { UserRoomRelation } from './modules/user-room-relation/user-room-relation.entity';
+import { Mission } from './modules/mission/mission.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'postgres',
       port: 5432,
       username: 'timeforgreen',
       password: 'timeforgreenpassword',
       database: 'timeforgreen',
-      entities: [User, Room, UserRoomRelation],
+      entities: [User, Room, UserRoomRelation, Mission],
       synchronize: true,
     }),
   ],
